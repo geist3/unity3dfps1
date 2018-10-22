d15440ec1d6746acd51b12c0601a102842f0b837
John Cockrell <jcockrell@gmail.com>
2018-10-22 21:31:30 +0100

    show ammo count, ammo runs out

---
+++ 3d fps zenva/Assets/Project/Scripts/Game/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    [Header("Game")]
+    public Player player;
+
+    [Header("UI")]
+    public Text ammoText;
+
+    private string oldText = "";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (oldText != ammoText.text)
+        {
+            ammoText.text = "Ammo: " + player.Ammo;
+        }
+	}
+}
--- 3d fps zenva/Assets/Project/Scripts/Game/Player.cs
+++ 3d fps zenva/Assets/Project/Scripts/Game/Player.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+
+    [Header("Visuals")]
     public Camera playerCamera;
 
-	// Use this for initialization
-	void Start () {
-		
+    [Header("GamePlay")]
+    public int initialAmmo = 10;
+    public int Ammo { get { return ammo; } }
+    private int ammo;
+
+    // Use this for initialization
+    void Start () {
+        ammo = initialAmmo;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(0))
+        if (ammo > 0 && Input.GetMouseButtonDown(0))
         {
+            ammo--;
             GameObject bulletObject = ObjectPoolingManager.Instance.GetBullet();
             bulletObject.transform.position = playerCamera.transform.position;
             bulletObject.transform.forward = playerCamera.transform.forward;

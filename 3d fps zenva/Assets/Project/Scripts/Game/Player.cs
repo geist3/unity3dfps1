4754600961394e2c31a8af6b11b2a826d04590a5
John Cockrell <jcockrell@gmail.com>
2018-10-24 20:54:15 +0100

    working on spike enemy

--- 3d fps zenva/Assets/Project/Scripts/Game/Player.cs
+++ 3d fps zenva/Assets/Project/Scripts/Game/Player.cs
@@ -6,6 +6,9 @@ public class Player : MonoBehaviour {
 
     private int ammo;
     private int health;
+    private float hurtDuration = 0.5f;
+    private bool isHurt = false;
+    public float knockbackForce = 10;
 
     [Header("Visuals")]
     public Camera playerCamera;
@@ -14,6 +17,7 @@ public class Player : MonoBehaviour {
     [Header("GamePlay")]
     public int initialHealth = 100;
     public int initialAmmo = 10;
+    public float knockback = 10;
     public int Ammo { get { return ammo; } }
     public int Health { get { return health; } }
 
@@ -38,10 +42,28 @@ public class Player : MonoBehaviour {
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
         AmmoCrate ammoCrate = hit.collider.gameObject.GetComponent<AmmoCrate>();
+        Enemy enemy = hit.collider.gameObject.GetComponent<Enemy>();
         if (ammoCrate != null)
         {
             ammo += ammoCrate.ammo;
             Destroy(ammoCrate.gameObject);
+        } else if(enemy != null)
+        {
+            if (!isHurt)
+            {
+                health -= enemy.damage;
+                isHurt = true;
+                Vector3 hurtDirection = (transform.position - enemy.transform.position).normalized;
+                Vector3 knockBackDirection = hurtDirection + Vector3.up;
+                GetComponent<Rigidbody>().AddForce(knockBackDirection * knockbackForce);
+                StartCoroutine(HurtRoutine());
+            }
         }
+    }
+
+    IEnumerator HurtRoutine()
+    {
+        yield return new WaitForSeconds(hurtDuration);
+        isHurt = false;
     }
 }
